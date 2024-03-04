9ad20f86fca2856fffd8471d4dc693e1de59f2f6
madmasyhur2 <muhammadmasyhur2@gmail.com>
2024-03-04 08:35:19 +0700

    sale

---
+++ database/migrations/2024_03_01_040232_create_sales_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('cashier_id');
+            $table->unsignedBigInteger('membership_id');
+            $table->string('sold_to');
+            $table->boolean('is_cash')->default(true);
+            $table->dateTime('sold_at');
+            $table->integer('total-amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales');
+    }
+};
