fee77f71c65efc60d620c4897c539180078f7640
Oecophyllaa <rangga.godzila@gmail.com>
2024-03-06 11:58:50 +0700

    add bootstrap5 & login view

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="{{ asset('backend/bootstrap/css/bootstrap.css') }}" rel="stylesheet" />
+</head>
+
+<body>
+    <h1>Hello, world!</h1>
+
+
+    <script src="{{ asset('backend/bootstrap/js/bootstrap.bundle.js') }}"></script>
+</body>
+
+</html>
